768794b4ee65788994c025a865301af167a2dbc1
Angelica <Angelica137@users.noreply.github.com>
2020-10-12 22:27:06 +0100

    print total repos

--- python_repos.py
+++ python_repos.py
@@ -6,8 +6,10 @@ url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
 headers = {'Accept': 'application/vnd.github.v3+json'}
 r = requests.get(url, headers=headers)
 print(f"Status code: {r.status_code}")
+
 # Store API response in a variable.
 response_dict = r.json()
+print(f"Total repositories: {response_dict['total_count']}")
 
 # Process results.
 print(response_dict.keys())
